77fbc6d4e42f99539ce421efe43439597175b9df
Heiko Schaefer <heiko@rangun.de>
2018-02-03 08:36:24 +0100

    building Huffman nodes

--- src/huffman.h
+++ src/huffman.h
@@ -3,26 +3,42 @@
 
 #include <algorithm>
 #include <vector>
+#include <deque>
 
 namespace huffman {
 
 template<class C, class P = double>
 class huffman {
-private:
+public:
 	typedef C character_type;
 	typedef P probability_type;
+
+private:
 
 	typedef class _alphabet_entry {
 	public:
 
-		_alphabet_entry(const character_type& c, const probability_type& p) :
-			m_character(c), m_probability(p) {}
+		_alphabet_entry(const character_type& c, const probability_type& p, bool a = false) :
+			m_active(a), m_character(c), m_probability(p) {}
+
+		void setActive(std::size_t p = 0u) {
+			m_active = p;
+		}
+
+		std::size_t active() const {
+			return m_active;
+		}
+
+		character_type character() const {
+			return m_character;
+		}
 
 		probability_type probability() const {
 			return m_probability;
 		}
 
 	private:
+		std::size_t m_active;
 		character_type m_character;
 		probability_type m_probability;
 	} ALPHABET_ENTRY;
@@ -41,8 +57,24 @@ private:
 
 		_column_entry(const column_entry_type& e) : m_entry(e) {}
 
+		void setActive(std::size_t p) {
+			m_entry.setActive(p);
+		}
+
+		std::size_t active() const {
+			return m_entry.active();
+		}
+
+		character_type character() const {
+			return m_entry.character();
+		}
+
 		probability_type probability() const {
 			return m_entry.probability();
+		}
+
+		friend bool operator==(const _column_entry& a, const _column_entry& b) {
+			return a.character() == b.character();
 		}
 
 		friend bool operator<(const _column_entry& x, const _column_entry& y) {
@@ -54,13 +86,23 @@ private:
 	} COLUMN_ENTRY;
 
 	typedef std::vector<COLUMN_ENTRY> COLUMN;
-	typedef std::vector<COLUMN> COLUMNS;
+
+	typedef struct {
+		typedef std::deque<character_type> NAME;
+		std::size_t row;
+		NAME name;
+		probability_type probability;
+	} NODE;
 
 public:
-	explicit huffman(const ALPHABET& a) : m_columns(build_table(a)) {}
 
-	COLUMNS table() const {
-		return m_columns;
+	typedef std::vector<COLUMN> COLUMNS;
+	typedef std::vector<NODE> NODES;
+
+	explicit huffman(const ALPHABET& a) : m_nodes(build_nodes(build_table(a))) {}
+
+	NODES nodes() const {
+		return m_nodes;
 	}
 
 private:
@@ -77,6 +119,8 @@ private:
 			typename COLUMN::value_type minima[2];
 
 			std::partial_sort_copy(std::begin(col), std::end(col), minima, minima + 2);
+			std::find(std::begin(*i), std::end(*i), minima[0])->setActive(1);
+			std::find(std::begin(*i), std::end(*i), minima[1])->setActive(2);
 
 			c.push_back(COLUMN());
 
@@ -95,8 +139,40 @@ private:
 		return c;
 	}
 
+	NODES build_nodes(const COLUMNS &t) {
+
+		NODES n;
+		std::size_t p = 0u;
+
+		for(auto i(std::begin(t.front())); i != std::end(t.front()); ++i, ++p) {
+			n.push_back(NODE { p, typename NODE::NAME { i->character() },
+				probability_type(i->probability()) });
+		}
+
+		typename NODE::NAME pname;
+
+		for(auto i(std::begin(t)); i != std::end(t) - 1u; ++i, ++p) {
+
+			n.push_back(NODE { p, typename NODE::NAME(std::begin(pname), std::end(pname)),
+				probability_type((i + 1u)->back().probability()) });
+
+			for(auto j(std::begin(*i)); j != std::end(*i); ++j) {
+				if(j->active() == 1u) {
+					n.back().name.push_front(j->character());
+				} else if(j->active() == 2u) {
+					n.back().name.push_back(j->character());
+				}
+			}
+
+			pname.clear();
+			pname.insert(std::end(pname), std::begin(n.back().name), std::end(n.back().name));
+		}
+
+		return n;
+	}
+
 private:
-	COLUMNS m_columns;
+	NODES m_nodes;
 };
 
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,30 @@
 #include <cstdlib>
+#include <iterator>
 #include <iostream>
 
 #include "huffman.h"
 
 int main(int, char **) {
 
-	std::cout << "Stages: " <<
-		huffman::huffman<char>({
-		huffman::huffman<char>::value_type('A', 0.12),
-		huffman::huffman<char>::value_type('E', 0.42),
-		huffman::huffman<char>::value_type('I', 0.09),
-		huffman::huffman<char>::value_type('O', 0.30),
-		huffman::huffman<char>::value_type('U', 0.07)
-	}).table().size() << std::endl;
+	typedef huffman::huffman<char> HUFFMAN;
+
+	HUFFMAN::NODES n(HUFFMAN({
+		HUFFMAN::value_type('A', 0.12),
+		HUFFMAN::value_type('E', 0.42),
+		HUFFMAN::value_type('I', 0.09),
+		HUFFMAN::value_type('O', 0.30),
+		HUFFMAN::value_type('U', 0.07)
+	}).nodes());
+
+	std::cout << "Nodes: " << n.size() << std::endl;
+
+	for(auto i(std::begin(n)); i != std::end(n); ++i) {
+
+		std::cout << (i->row + 1u) << "; ";
+		std::copy(std::begin(i->name), std::end(i->name),
+              std::ostream_iterator<HUFFMAN::character_type>(std::cout, ""));
+        std::cout << "; " << i->probability << std::endl;
+	}
 
 	return EXIT_SUCCESS;
 }
